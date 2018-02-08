891e888004b1d25f5c75df3c90ff4f17f5d5005b
William Lewis <william.lewis@wework.com>
2018-02-08 21:27:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors)
+def display_rainbow(array)
   puts #the colors in the array argument
 end
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
