589194e80e0c1e7bd4074bdd9af735bd814f1c64
William Lewis <william.lewis@wework.com>
2018-02-08 21:29:21 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,12 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
   puts "The first letter in the alphabet is: #{array[0]}"
+  puts "The first letter in the alphabet is: #{array[0]}"
+  puts "The first letter in the alphabet is: #{array[0]}"
+  puts "The first letter in the alphabet is: #{array[0]}"
+  puts "The first letter in the alphabet is: #{array[0]}"
+  puts "The first letter in the alphabet is: #{array[0]}"
+  puts "The first letter in the alphabet is: #{array[0]}"
 end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
