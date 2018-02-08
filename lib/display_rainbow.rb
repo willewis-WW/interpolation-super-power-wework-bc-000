fc3610912063c4bc72834458d712a03e9ed1347c
William Lewis <william.lewis@wework.com>
2018-02-08 21:33:54 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -11,10 +11,10 @@ end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-  puts "R: #{array[0]},"
-  puts "O: #{array[1]},"
-  puts "Y: #{array[2]},"
-  puts "G: #{array[3]},"
-  puts "B: #{array[4]},"
-  puts "I: #{array[5]},"
-  puts "V: #{array[6]}"
+#  puts "R: #{array[0]},"
+#  puts "O: #{array[1]},"
+#  puts "Y: #{array[2]},"
+#  puts "G: #{array[3]},"
+#  puts "B: #{array[4]},"
+#  puts "I: #{array[5]},"
+#  puts "V: #{array[6]}"
