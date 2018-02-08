0edfe88164dd1966a3f22d24eedb3d28d773f720
William Lewis <william.lewis@wework.com>
2018-02-08 21:32:58 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
-  puts "R: #{array[0]}"
-  puts "O: #{array[1]}"
-  puts "Y: #{array[2]}"
-  puts "G: #{array[3]}"
-  puts "B: #{array[4]}"
-  puts "I: #{array[5]}"
+  puts "R: #{array[0]},"
+  puts "O: #{array[1]},"
+  puts "Y: #{array[2]},"
+  puts "G: #{array[3]},"
+  puts "B: #{array[4]},"
+  puts "I: #{array[5]},"
   puts "V: #{array[6]}"
 end
 
